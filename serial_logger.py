2d424ede03ea9a696010bd1fb9e4b58901e41d78
polentozer <markus.tadej@gmail.com>
2017-04-15 21:37:18 +0200

    Added shebang and set logging time to 10 min

--- serial_logger.py
+++ serial_logger.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python
+
 # Mandatory imports
 import serial
 import time
@@ -18,12 +20,12 @@ ser = serial.Serial(port=uno, baudrate=9600)
 
 # Setting filename of .log file
 exp_date = dt.date.today()
-exp_time = time.strftime('%H:%M')
-fname = str(exp_date) + '-' + str(exp_time) + '.log'
+exp_time = time.strftime('%H-%M')
+fname = str(exp_date) + '_' + str(exp_time) + '.log'
 
-# Setting experiment duration
+# Setting experiment duration (600 = 10 min)
 start = int(time.time())
-end = start + 100
+end = start + 600
 
 # Wait a sec then print and throw away first line; likely incomplete
 time.sleep(1)
@@ -31,7 +33,6 @@ print(ser.readline())
 
 # Setting read/write loop
 while start < end:
-    start += 1
+    start += 0.1
     x = str(ser.readline()).replace('\\t', ',')
-    # print(x[2:-5])
     writer(fname, x[2:-5])
