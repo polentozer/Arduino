6aefc3c871363cea3e7697f910c40644a26bdb13
polentozer <markus.tadej@gmail.com>
2017-04-23 14:24:39 +0200

    Message for manual interrupt

--- logg3r.py
+++ logg3r.py
@@ -24,23 +24,27 @@ exp_time = time.strftime('%H-%M')
 fname = str(exp_date) + '_' + str(exp_time) + '.log'
 
 # Setting read/write loop + throw away first few lines
-print('Python3.x logging script...')
-n = 0
-while n < 10:
-    n += 1
-    x = ser.readline()
-    time.sleep(0.2)
-else:
-    # Setting logging duration (600 ~ 10 min)
-    with open(fname, 'a') as fout:
-        fout.write('TIME.MS,AN_READ,VOLTAGE,pH_VALUE\n')
-    start = int(time.time())
-    end = start + 120
-    print('Start of logging: {}.'.format(time.strftime('%H:%M:%S')))
-    while start < end:
-        # Increment has to be the same as Arduino delay
-        start += 0.2
-        x = str(ser.readline()).replace('\\t', ',')
-        writer(fname, x[2:-5])
+try:
+    print('Python3.x logging script...')
+    n = 0
+    while n < 10:
+        n += 1
+        x = ser.readline()
+        time.sleep(0.2)
     else:
-        print('End of logging: {}.'.format(time.strftime('%H:%M:%S')))
+        # Setting logging duration (600 ~ 10 min)
+        with open(fname, 'a') as fout:
+            fout.write('TIME.MS,AN_READ,VOLTAGE,pH_VALUE\n')
+        start = int(time.time())
+        end = start + 1200
+        print('Start of logging: {}.'.format(time.strftime('%H:%M:%S')))
+        while start < end:
+            # Increment has to be the same as Arduino delay
+            start += 0.2
+            x = str(ser.readline()).replace('\\t', ',')
+            writer(fname, x[2:-5])
+            print(time.strftime('%H:%M:%S'))
+        else:
+            print('End of logging: {}.'.format(time.strftime('%H:%M:%S')))
+except KeyboardInterrupt:
+    print('Exiting with manual interrupt...')
